68f22b03dc6d1458d6ce2ec55bf9fdea6fbcaaab
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-23 08:20:29 -0700

    Sett up LocalStrategy

--- app.js
+++ app.js
@@ -33,6 +33,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await user.findOne( { username: username });
+            if (!user) {
+                return done(null, false,  {message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        }
+    })
+)
+
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
